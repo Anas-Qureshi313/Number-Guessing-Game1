a419a32f4fa9542be0b42e09f40895ea56042410
Anas-Qureshi313 <anusqureshi29@gmail.com>
2024-04-09 02:48:49 -0700

    created Number guessing game

--- README.md
+++
@@ -1,2 +0,0 @@
-# Number-Guessing-Game1
-This is Number Guessing Game based on Typescript, Nodejs and Inquirer
---
+++ index.ts
@@ -0,0 +1,23 @@
+import inquirer from "inquirer";
+
+// steps: 
+//     1) generate a random number by machine
+//     2) enter your guessed number
+//     3) compare your number with random number and show the result
+
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+
+const awnswer = await inquirer.prompt(
+    [
+        {message: "Please Guess a Number", name: "userguessedNumber", type: "number",},
+    ]
+    );
+    
+    if( awnswer.userguessedNumber === randomNumber) {
+    console.log("Congratulations! You Guessed the Right Number");
+}
+
+else{ console.log("Oops! You Guessed the Wrong Number");
+}
+console.log(randomNumber); 
+// console.log(awnswer);
